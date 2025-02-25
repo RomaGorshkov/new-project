bb005e437e4526c84cbecfa077f159c4cc92efab
Roman <roman.gorshkov19@gmail.com>
2025-02-25 10:54:46 +0200

    first commit

---
+++ src/pages/UsersEditPage/UsersEditPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import UsersEdit from "../../components/business/UsersEdit/UsersEdit";
+
+const UsersEditPage: React.FC = () => {
+  return <UsersEdit />;
+};
+
+export default UsersEditPage;
---
+++ src/pages/UsersPage/UsersPage.tsx
@@ -0,0 +1,7 @@
+import Users from '../../components/business/Users/Users';
+
+const UsersPage = () => {
+    return <Users />;
+};
+
+export default UsersPage;
